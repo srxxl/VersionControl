7cbb9c12ddfc0d5efa52d3f62469a7ed62ab974b
Sarah <sarahrxxl@gmail.com>
2023-04-18 18:02:50 +0800

    Inserted new code

--- Maze.py
+++ Maze.py
@@ -65,21 +65,22 @@ if __name__ == "__main__":
             print("You fall into the chasm of doom") # Out of bounds.
             
         else:
-            if grid[currentRow][currentCol] == EMPTY:
+            cell = grid[currentRow][currentCol] 
+            if cell == EMPTY:
                 grid[currentRow][currentCol] = VISITED
-                
-            elif grid[currentRow][currentCol] == WALL:
-                done = True
-                print("You stumble blindly into a solid concrete wall") # Hit wall.
 
-            elif grid[currentRow][currentCol] == END:
+            elif cell == WALL: 
                 done = True
-                solved = True
-                print("Solved!") # Solved.
+                print("You stumble blindly into a solid concrete wall.")
+
+            elif cell == END: 
+                done = True 
+                solved = True 
+                print("SOLVED!")
                 
             else:
                 pass # Do nothing
-        
+
         charIndex += 1
     # end-while
 
